9e2709fa076600a8c8d0c64fc49dda3148cb4ac2
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:32:54 -0300

    Create multiplos2.cpp

---
+++ BEECROWD/beecrowd desafios/multiplos2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+int n1, n2;
+cin >> n1 >> n2;
+if (n2 % n1 == 0) {
+cout << "Sao Multiplos" << endl;
+}
+else {
+    cout << "Nao sao Multiplos" << endl;
+}
+return 0;
+}
