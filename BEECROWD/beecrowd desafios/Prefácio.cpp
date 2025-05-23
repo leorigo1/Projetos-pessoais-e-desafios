9440541501be0b370e929724d311cccdd29736f0
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:33:43 -0300

    Create Prefácio.cpp

---
+++ BEECROWD/beecrowd desafios/Prefácio.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int q = a / b;
+    int r = a % b;
+
+    if (r < 0) {
+        if (b > 0) {
+            q -= 1;
+            r += b;
+        } else {
+            q += 1;
+            r -= b;
+        }
+    }
+
+    cout << q << " " << r << endl;
+
+    return 0;
+}
