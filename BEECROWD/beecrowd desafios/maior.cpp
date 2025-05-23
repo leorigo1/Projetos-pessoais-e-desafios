2f69e773851901ec57b6eaed17e99897c4414351
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:32:39 -0300

    Create maior.cpp

---
+++ BEECROWD/beecrowd desafios/maior.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main () {
+int a,b,c,d,e,f;
+int maior;
+cin >> a >> b >> c >> d >> e >> f;
+maior = max({a,b,c,d,e,f});
+cout << maior << endl;
+
+
+
+return 0;
+}
