52e16eb6c2228ef672d49d636c5555d745e522cc
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:33:33 -0300

    Create paraprova.cpp

---
+++ BEECROWD/beecrowd desafios/paraprova.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main () {
+
+int a = 1, b = 2, c = 3;
+ a = a + 1;
+  b = a;
+   a = 1;
+    c = a + b;
+     cout << c << endl;
+      cout << b << endl;
+       cout << a << endl;
+
+return 0;
+}
