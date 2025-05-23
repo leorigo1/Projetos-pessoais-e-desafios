c5c73059203e9c7447adfcca0e5cefc926c68d41
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:31:41 -0300

    Create ddd2.cpp

---
+++ BEECROWD/beecrowd desafios/ddd2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main () {
+int valor;
+cin >> valor;
+if (valor == 11) {
+    cout << "Sao Paulo" << endl;
+}
+if (valor == 61) {
+    cout << "Brasilia" << endl;
+}
+if (valor == 71) {
+    cout << "Salvador" << endl;
+}
+if (valor == 21) {
+    cout << "Rio de Janeiro" << endl;
+}
+if (valor == 32) {
+    cout << "Juiz de Fora" << endl;
+}
+if (valor == 19) {
+    cout << "Campinas" << endl;
+}
+if (valor == 27) {
+    cout << "Vitoria" << endl;
+}
+if (valor == 31) {
+    cout << "Belo Horizonte" << endl;
+}
+return 0;
+}
