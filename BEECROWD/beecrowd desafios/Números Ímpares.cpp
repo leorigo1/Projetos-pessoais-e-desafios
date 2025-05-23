1574fb622b0e5ae9a61183841efd09eb8203fe47
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:33:01 -0300

    Create Números Ímpares.cpp

---
+++ BEECROWD/beecrowd desafios/Números Ímpares.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main () {
+int i , x, valores;
+cin >> x;
+
+for (i = 1; i <= x; i+= 2) {
+    cout << i << endl;
+}
+
+return 0;
+}
