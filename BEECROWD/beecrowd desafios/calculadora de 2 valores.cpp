d20712c7f7a8a8927825b74fa63640c785cc0fb7
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:31:30 -0300

    Create calculadora de 2 valores.cpp

---
+++ BEECROWD/beecrowd desafios/calculadora de 2 valores.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ///definicao das variaveis
+  int a;
+  int b;
+  double altura,temperatura;
+  string Nome_pessoa;
+
+  cout << "Calculadora do Leonardo" << endl;
+  cout << "digite o primeiro valor" << endl;
+   cin >> a;
+   cout << "digite o segundo valor" << endl;
+   cin >> b;
+
+  ///Entrada de dados
+
+
+  ///processamento
+  int total = a + b;
+
+  cout << "A soma dos 2 valores: " << total << endl;
+
+  return 0;
+
+}
