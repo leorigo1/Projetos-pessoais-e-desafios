f4e82fb8fc757c504213f1869aaa6d630fd8fc6b
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:43:31 -0300

    Create Soma de Ímpares Consecutivos III.cpp

---
+++ BEECROWD/beecrowd desafios/Soma de Ímpares Consecutivos III.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+int val;
+cin >> val;
+
+for (int i=0; i<val; i++) {
+int x,y;
+cin >> x >> y;
+
+if (x % 2 !== 0) {
+    x = x + 1;
+}
+for (int a=x; a<=; a=a+2) {
+    int resx = resx
+}
+
+}
+
+
+
+return 0;
+}
