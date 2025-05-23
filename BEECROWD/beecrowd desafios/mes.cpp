a541edba9f3029db28ae79ac246302e9b7b36896
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:32:49 -0300

    Create mes.cpp

---
+++ BEECROWD/beecrowd desafios/mes.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main () {
+int valor;
+cin >> valor;
+switch (valor) {
+
+case 1:
+cout << "January" << endl;
+
+case 2:
+cout << "February" << endl;
+
+case 3:
+cout << "March" << endl;
+
+case 4:
+cout << "April" << endl;
+
+case 5:
+cout << "May" << endl;
+
+case 6:
+cout << "June" << endl;
+
+case 7:
+cout << "July" << endl;
+
+case 8:
+cout << "August" << endl;
+
+case 9:
+cout << "September" << endl;
+
+case 10:
+cout << "October" << endl;
+
+case 11:
+cout << "November" << endl;
+
+case 12:
+cout << "December" << endl;
+}
+return 0;
+}
