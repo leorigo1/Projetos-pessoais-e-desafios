7cfd8e1596069c331207dec47629264d0fe09359
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:31:33 -0300

    Create contas matematicas do professor.cpp

---
+++ BEECROWD/beecrowd desafios/contas matematicas do professor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <locale>
+using namespace std;
+int main () {
+setlocale(LC_ALL,"");
+cout << "O resultado é: " << 6 / 2 % 2 << endl;
+
+
+
+return 0;
+
+}
+
