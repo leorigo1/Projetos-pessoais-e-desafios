c32578996bacaf01b5ea05e8238f5119be2c51b9
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:32:40 -0300

    Create media1.cpp

---
+++ BEECROWD/beecrowd desafios/media1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main () {
+
+double A,B, soma;
+cin >> A >> B;
+soma = (A*3.5 + B*7.5) / 11;
+cout << fixed << setprecision(5) << "MEDIA = " << soma << endl;
+
+}
