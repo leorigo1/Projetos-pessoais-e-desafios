b593847a0a61c043a6c4babdd309f34118d7b264
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:31:44 -0300

    Create distancia.cpp

---
+++ BEECROWD/beecrowd desafios/distancia.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main () {
+int valor, resultado;
+cin >> valor;
+resultado = valor * 2;
+cout << resultado << " minutos" << endl;
+return 0;
+}
