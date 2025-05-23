085d1aa36528335007056fc722875cbf8a34ecda
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:33:40 -0300

    Create pneu.cpp

---
+++ BEECROWD/beecrowd desafios/pneu.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main () {
+int n1, n2 , resultado;
+cin >> n1 >> n2;
+resultado = n1 - n2;
+cout << resultado << endl;
+return 0;
+}
