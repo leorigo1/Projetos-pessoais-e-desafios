2c5990e7a99aed672b26994293a095e5260c9040
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-05-23 13:43:16 -0300

    Create tipos de triangulo.cpp

---
+++ BEECROWD/beecrowd desafios/tipos de triangulo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main () {
+float a,b,c;
+cin >> a >> b >> c;
+if (a <= (b + c )) {
+ cout << "NAO FORMA TRIANGULO" << endl;
+}
+
+
+return 0;
+}
